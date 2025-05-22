75afb16025adecacacc15c20b8efc2c47df316ca
VimalaSellappan <Vimala.Sellappan@stud.sundsgarden.se>
2025-05-22 11:58:59 +0200

    Home/Register page created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
-import DogGameLogic from './components/DogGameLogic';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomeRegisterPage from './components/HomeRegisterpage'; 
 
 function App() {
   return (
-      <div className="App">
-      < DogGameLogic />
-      </div>
-    
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomeRegisterPage />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/components/HomeRegisterpage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './HomeRegisterpage.css'
+function HomeRegisterpage() {
+  return (
+    <div className="home-container">
+      <div className="welcome-part">
+            <h1>Welcome to Dog Matcher</h1>
+            <p className='sub-head'>Join us to Play!</p>
+        </div>
+      <div className='bottom-part'>
+    <div className="register-container">
+        
+        <div className="form-section">
+        <h2>Sign Up here</h2>
+        <form className='form-container'>
+          <div className='form-content'>
+            <input type="text" placeholder="Full Name" required />
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+          </div>
+          <button type="submit">Create Account</button>
+        </form>
+        <p className="signin">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </div>
+    </div>
+    <div className='image-container'>
+      <img src='../images/dog.png'/>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default HomeRegisterpage;
